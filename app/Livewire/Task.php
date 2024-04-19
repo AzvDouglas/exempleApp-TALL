afcfafca206e2ecb38414bf2be26b6d77ebaa88a
azvdouglas <douglasbep2014@gmail.com>
2024-04-19 04:15:58 -0300

    Task.Create

---
+++ app/Livewire/Task.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+use App\Models\Task as TaskModel;
+
+class Task extends Component
+{
+    public $tasks;
+    public $title;
+    public $description;
+
+    protected $rules = [
+        'title'       => 'required|max:40',
+        'description' => 'required|max:400',
+    ];
+
+    public function mount(): void
+    {
+        $this->tasks = TaskModel::orderBy('id', 'desc')->get();
+    }
+
+    public function render()
+    {
+        return view('livewire.task');
+    }
+
+    public function save()
+    {
+        $this->validate();
+
+        $task = new TaskModel();
+        $task->title = $this->title;
+        $task->description = $this->description;
+        $task->save();
+        //limpar o formulário
+        $this->title = '';
+        $this->description = '';
+        $this->tasks = TaskModel::get();
+        session()->flash('message', 'Tarefa adicionada!');
+    }
+}
---
+++ resources/views/livewire/task.blade.php
@@ -0,0 +1,51 @@
+<div class="flex-column justify-center w-3/4">
+    <form wire:submit.blur="save" class="p-4">
+        <div class="mb-4">
+            <input wire:model.blur="title" class="p-2 bg-gray-200 w-full" type="text"  placeholder="Tarefa...">
+            @error("title")<div class="mt-1 text-red-600 text-sm">{{$message}}</div>@enderror
+        </div>
+        <div class="mb-4">
+            <textarea wire:model.blur="description" class="p-2 bg-gray-200 w-full" placeholder="Descrição da tarefa"></textarea>
+            @error("description")<div class="mt-1 text-red-600 text-sm">{{$message}}</div>@enderror
+        </div>
+        <button type="submit" class="bg-indigo-700 text-white font-bold w-full rounded shadow p-2">Guardar</button>
+    </form>
+
+    @if(session()->has('message'))
+        <h3 class="bg-blue-400 font-bold mb-4 p-2 rounded text-center text-sm text-white">{{session('message')}}</h3>
+    @endif
+    <table class="shadow-md ">
+        <thead>
+        <tr class="bg-gray-200 text-gray-600 uppercase text-sm">
+            <th class="py-3 px-6 text-left">&nbsp;</th>
+            <th class="py-3 px-6 text-left">Tarefas</th>
+            <th class="py-3 px-6 text-left">Descrição</th>
+            <th class="py-3 px-6 text-left">&nbsp;</th>
+        </tr>
+        </thead>
+        <tbody class="text-gray-600">
+        @forelse($tasks as $task)
+            <tr class="border-b border-gray-200">
+                <td class="px-4 py-2"><input type="checkbox"></td>
+                <td class="px-4 py-2">{{$task->title}}</td>
+                <td class="px-4 py-2">{{$task->description}}</td>
+                <td class="px-4 py-2">
+                    <button type="button" class="bg-indigo-400 px-2 py-1 text-white text-xs rounded">Editar</button>
+                    <button type="button" class="bg-red-500 px-2 py-1 text-white text-xs rounded">Eliminar</button>
+                </td>
+            </tr>
+        @empty
+            <h3>Sem Tarefas Registradas.</h3>
+        @endforelse
+        </tbody>
+    </table>
+
+    <form wire:submit.blur="save" class="p-4">
+        <div class="mb-4">
+            <input wire:model="title" class="p-2 bg-gray-200 w-full" type="text"  placeholder="Tarefa...">
+        </div>            <div class="mb-4">
+            <textarea wire:model="description" class="p-2 bg-gray-200 w-full" placeholder="Descrição da tarefa"></textarea>
+        </div>
+        <button type="submit" class="bg-indigo-700 text-white font-bold w-full rounded shadow p-2">Guardar</button>
+    </form>
+</div>
