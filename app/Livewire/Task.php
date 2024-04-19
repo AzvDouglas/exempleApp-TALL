2b7991b04c93347472bb6eb97800e1d0e63831c4
azvdouglas <douglasbep2014@gmail.com>
2024-04-19 11:19:07 -0300

    Task.Edit

--- app/Livewire/Task.php
+++ app/Livewire/Task.php
@@ -9,6 +9,7 @@ class Task extends Component
 {
     public $tasks;
     public $title;
+    public $taskId;
     public $description;
 
     protected $rules = [
@@ -19,6 +20,8 @@ class Task extends Component
     public function mount(): void
     {
         $this->tasks = TaskModel::orderBy('id', 'desc')->get();
+        $task = new TaskModel();
+
     }
 
     public function render()
@@ -30,18 +33,33 @@ class Task extends Component
     {
         $this->validate();
 
-        $task = new TaskModel();
-        $task->title = $this->title;
-        $task->description = $this->description;
-        $task->save();
+        if ($this->taskId) {
+            $task = TaskModel::find($this->taskId);
+            $task->title = $this->title;
+            $task->description = $this->description;
+            $task->save();
+        } else {
+            $task = new TaskModel();
+            $task->title = $this->title;
+            $task->description = $this->description;
+            $task->save();
+        }
+
         //limpar o formulário
         $this->title = '';
         $this->description = '';
+        $this->taskId = null;
 
-        $this->dispatch('taskCreated', 'Tarefa adicionada!');
-        $this->mount();
+        $this->tasks = TaskModel::orderBy('id', 'desc')->get();
     }
-
+    public function edit(TaskModel $task): void
+    {
+        $this->task = $task;
+        $this->taskId = $task->id;
+        $this->description = $task->description;
+        $this->title = $task->title;
+        //$this->mount();
+    }
     public function updated()
     {
         $this->validate();
